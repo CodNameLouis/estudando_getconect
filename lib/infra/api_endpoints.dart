465c922a2e4321e338dd958dc4322cd46cae95db
Luan Nunes <97055040+CodNameLouis@users.noreply.github.com>
2022-07-19 15:19:59 -0400

    ADD Created infra structure

---
+++ lib/infra/api_endpoints.dart
@@ -0,0 +1,9 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  static const base = "http://ec2-54-80-244-185.compute-1.amazonaws.com:3333/api/v1";
+
+
+  static const zipCode = '$base/zip-code/85811490';
+
+}
